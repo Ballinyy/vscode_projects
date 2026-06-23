a3b6c3acbab22e154e30c365be75cc407b8cf8e3
Ballinyy <alessandro12.ballini@gmail.com>
2026-06-23 21:41:46 +0200

    typos

--- manim_proj/curves.py
+++ manim_proj/curves.py
@@ -3,5 +3,5 @@ from manim import *
 class CreateCircle(Scene):
     def construct(self):
         circle = Circle()
-        circle.set_fill(RED, opacity=0.5)
+        circle.set_fill(RED, opacity=0.6)
         self.play(Create(circle))
